ee2a495895eae8b0bcb14789e67e8a4ea5e05df6
Shrivishnu <srivishnu31@gmail.com>
2020-08-02 16:12:04 +0530

    Update the package for general use

--- index.js
+++ index.js
@@ -3,37 +3,44 @@ const fs = require("fs");
 const path = require("path");
 const ytdl = require("ytdl-core");
 
-const url =
-  "https://www.youtube.com/playlist?list=PLzufeTFnhupwghcSnHZs9qHxSZYyY-djm";
-
 const printProgress = (message) => {
   process.stdout.clearLine();
   process.stdout.cursorTo(0);
   process.stdout.write(message);
 };
 
-const download = (url, folder, title) => {
+const download = (url, folder, title, options) => {
+  if (!fs.existsSync(`download`))
+    fs.mkdirSync(`download`, (err) => {
+      if (err) {
+        console.log("Couldn't create folder download");
+        return;
+      }
+      console.log("Successfully created folder download");
+    });
   if (!fs.existsSync(`download/${folder}`))
     fs.mkdirSync(`download/${folder}`, (err) => {
-      if (err) console.log("Couldn't create folder");
-      console.log("successfully created folder");
+      if (err) {
+        console.log(`Couldn't create folder download/${folder}`);
+        return;
+      }
+      console.log(`Successfully created folder download/${folder}`);
     });
 
   const writer = fs.createWriteStream(
     path.resolve(__dirname, "download", folder, `${title}.mp4`)
   );
   let contentLength = 0;
-  ytdl(url, { quality: "18" })
+  ytdl(url, options)
     .on("info", (videoInfo, videoFormat) => {
       contentLength = Number(videoFormat.contentLength);
     })
     .on("progress", (byteReceived, totalByteReceived, totalByteFile) => {
       if (contentLength)
         printProgress(
-          `Completed ${(
-            (totalByteReceived / totalByteFile) *
-            100
-          ).toFixed(2)} %`
+          `Completed ${((totalByteReceived / totalByteFile) * 100).toFixed(
+            2
+          )} %`
         );
     })
     .pipe(writer);
@@ -43,16 +50,18 @@ const download = (url, folder, title) => {
   });
 };
 
-ytlist(url, ["url", "name"]).then(async (res) => {
-  const { name: foldername, playlist } = res.data;
-  for (let i = 0; i < playlist.length; i++) {
-    const { url, name: title } = playlist[i];
-    console.log(`Started ${title}`);
-    try {
-      await download(url, foldername.replace(/\|/g,'-'), title);
-    } catch (err) {
-      i--;
+module.exports = (url, options) => {
+  ytlist(url, ["url", "name"]).then(async (res) => {
+    const { name: foldername, playlist } = res.data;
+    for (let i = 0; i < playlist.length; i++) {
+      const { url, name: title } = playlist[i];
+      console.log(`Started ${title}`);
+      try {
+        await download(url, foldername.replace(/\|/g, "-"), title);
+      } catch (err) {
+        i--;
+      }
+      console.log(`\nCompleted ${title}`);
     }
-    console.log(`\nCompleted ${title}`);
-  }
-});
+  });
+};
