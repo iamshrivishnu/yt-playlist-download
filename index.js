08ab611155b1df9e838455c466275cb4d5876ee5
Shrivishnu <srivishnu31@gmail.com>
2020-08-02 16:55:14 +0530

    Separated Folder Creation, Handled File and Folder Name

--- index.js
+++ index.js
@@ -9,24 +9,27 @@ const printProgress = (message) => {
   process.stdout.write(message);
 };
 
-const download = (url, folder, title, options) => {
+const createFolders = (folder) => {
   if (!fs.existsSync(`download`))
     fs.mkdirSync(`download`, (err) => {
       if (err) {
-        console.log("Couldn't create folder download");
-        return;
+        console.error("Couldn't create folder download");
+        return 0;
       }
-      console.log("Successfully created folder download");
+      console.info("Successfully created folder download");
     });
   if (!fs.existsSync(`download/${folder}`))
     fs.mkdirSync(`download/${folder}`, (err) => {
       if (err) {
-        console.log(`Couldn't create folder download/${folder}`);
-        return;
+        console.error(`Couldn't create folder download/${folder}`);
+        return 0;
       }
-      console.log(`Successfully created folder download/${folder}`);
+      console.info(`Successfully created folder download/${folder}`);
     });
+  return 1;
+};
 
+const download = (url, folder, title, options) => {
   const writer = fs.createWriteStream(
     path.resolve(__dirname, "download", folder, `${title}.mp4`)
   );
@@ -53,15 +56,30 @@ const download = (url, folder, title, options) => {
 module.exports = (url, options) => {
   ytlist(url, ["url", "name"]).then(async (res) => {
     const { name: foldername, playlist } = res.data;
-    for (let i = 0; i < playlist.length; i++) {
-      const { url, name: title } = playlist[i];
-      console.log(`Started ${title}`);
-      try {
-        await download(url, foldername.replace(/\|/g, "-"), title);
-      } catch (err) {
-        i--;
+    const folder = await createFolders(foldername);
+    if (folder) {
+      for (let i = 0; i < playlist.length; i++) {
+        const { url, name: title } = playlist[i];
+        console.info(
+          `Started ${title.replace(/\||\?|\\|\/|\:|\*|"|\<|\>/g, "-")}`
+        );
+        try {
+          await download(
+            url,
+            foldername.replace(/\||\?|\\|\/|\:|\*|"|\<|\>/g, "-"),
+            title.replace(/\||\?|\\|\/|\:|\*|"|\<|\>/g, "-"),
+            options
+          );
+        } catch (err) {
+          console.error(err);
+          i--;
+        }
+        console.info(
+          `\nCompleted ${title.replace(/\||\?|\\|\/|\:|\*|"|\<|\>/g, "-")}`
+        );
       }
-      console.log(`\nCompleted ${title}`);
+    } else {
+      console.error("Could't create folder. Quititng...");
     }
   });
 };
